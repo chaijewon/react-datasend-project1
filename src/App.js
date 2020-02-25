2b17da6e9f201841626041acf2e233c2a18d5c01
chaijewon <vacnadjava@nate.com>
2020-02-25 14:54:00 +0900

    datasend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 import React,{Component} from 'react';
 class App extends Component{
+    constructor(props) {
+        super(props);
+        // 데이터를 받아서 저장
+        this.state={
+            movie:[],
+            isShow:false,
+            detail:{}
+        }
+    }
+    // JSON을 읽어 온다 ==> state에 저장
+    componentDidMount() {
 
+    }
+    // state에 저장된 데이터 출력
+    // 포스터를 클릭하면 => 데이터를 => MovieDetail 에 전송 (props)
+    /*
+        function App()
+
+        => App(값)
+     */
+    render() {
+
+    }
+}
+class MovieDetail extends Component{
+  
 }
 export default App;
